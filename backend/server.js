b4ad0e595f0492bc7b553238cf575d1c335ed650
venkateshkallu <venkateshkalluu@gmail.com>
2025-06-16 15:57:15 +0530

    Initial commit

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+// backend/server.js
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// Simple route
+app.get('/', (req, res) => {
+  res.send('API is working');
+});
+
+// Connect MongoDB
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(() => console.log('MongoDB connected'))
+  .catch(err => console.log(err));
+
+// Start server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
